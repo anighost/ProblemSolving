378f7ccfe2c0749c68d083e97d4c039cb02efbcd
Anirban Ghosh <anirbanghosh@MacBook-Air.local>
2019-04-12 23:13:41 -0700

    commit

---
+++ CodingPractice/Kata/src/main/java/problems/algo/array/RemoveDuplicate.java
@@ -0,0 +1,25 @@
+package problems.algo.array;
+
+/**
+ * 
+ * 	Given a sorted array nums, remove the duplicates in-place such that 
+ * 	each element appear only once and return the new length.
+
+	Do not allocate extra space for another array, 
+	you must do this by modifying the input array in-place with O(1) extra memory.
+
+ *
+ */
+public class RemoveDuplicate {
+
+    public int removeDuplicates(int[] nums) {
+    	
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
