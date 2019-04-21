129c94dd9ebe46773148864def9e6471118041e5
Anirban Ghosh <anirbanghosh@MacBook-Air.local>
2019-04-21 03:00:45 -0700

    commit

--- CodingPractice/Kata/src/main/java/problems/algo/backtrack/FindStringInMatrix2.java
+++ CodingPractice/Kata/src/main/java/problems/algo/backtrack/FindStringInMatrix2.java
@@ -2,18 +2,12 @@ package problems.algo.backtrack;
 
 public class FindStringInMatrix2 {
     
-    public int [][] visited;
-    public int[] rows = new int [] {0,0,-1,1};
-    public int[] cols = new int [] {1,-1,0,0};
-    
     
     public boolean exist(char[][] board, String word) {
-
-    	visited = new int[board.length][board[0].length];
        
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length ; j++) {
-            	boolean isMatch = search(board, 0, i, j, word);
+            	boolean isMatch = search(board, word, i, j, 0);
                 if (isMatch) {
                 	return true;
                 }
@@ -24,68 +18,23 @@ public class FindStringInMatrix2 {
         
     }
     
-    public boolean search(char[][] board, int pos, int row, int col, String word) {
+    public boolean search(char[][] board, String word, int row, int col, int pos) {
 
-    	//Validate inputs
-    	if (word == null || word.isEmpty() || board.length == 0) {
-    		return false;
-    	}
-    	
-        //Return in case of mismatch or cell already visited
-        if(board[row][col] != word.charAt(pos) 
-            || visited[row][col] > 0) {
+        if(row < 0 || col < 0 || row >= board.length || col >= board[row].length || pos >= word.length()
+                ||word.charAt(pos) != board[row][col]) {
             return false;
         }
-        
-        //At this point board[row][col] already matches with word.charAt(pos)
-        //Return if whole word is found
-        if(pos == word.length()-1) {
+        if(pos == word.length() - 1) {
             return true;
         }
-
-        //mark visited matrix
-        visited[row][col] = 1;
-        
-//        //check right
-//        if(col+1 < board[row].length 
-//            && search(board, pos+1, row, col+1, word)) {
-//            return true;
-//        }
-//        
-//        //check left
-//        if(col-1 >= 0 
-//            && search(board, pos+1, row, col-1, word)) {
-//            return true;
-//        }
-//
-//        //check up
-//        if(row-1 >= 0
-//            && search(board, pos+1, row-1, col, word)) {
-//            return true;
-//        }
-//
-//        //check down
-//        if(row+1 < board.length 
-//            && search(board, pos+1, row+1, col, word)) {
-//            return true;
-//        }
-        
-
-        //Check 4 directions
-        for (int k = 0; k < 4; k++) {
-        	int nrow = row + rows[k];
-        	int ncol = col + cols[k];
-        	
-        	if(nrow < board.length && nrow >= 0 && ncol < board[nrow].length && ncol >=0
-                  && search(board, pos+1, nrow, ncol, word)) {
-                  return true;
-              }        	
-        }
-        //backtrack and reset visited matrix
-        visited[row][col] = 0;
-        
+        board[row][col] = '0';
+        if(search(board, word, row + 1, col, pos + 1)) return true;
+        if(search(board, word, row, col + 1, pos + 1)) return true;
+        if(search(board, word, row, col - 1, pos + 1)) return true;
+        if(search(board, word, row - 1, col, pos + 1)) return true;
+        board[row][col] = word.charAt(pos);
         return false;
-        
+       
     }
     
 	public static void main(String[] args) {
--- CodingPractice/Kata/src/main/java/problems/algo/linkedlist/LRUCache.java
+++ CodingPractice/Kata/src/main/java/problems/algo/linkedlist/LRUCache.java
@@ -1,8 +1,109 @@
 package problems.algo.linkedlist;
 
-public class LRUCache {
-	//TODO
+import java.util.HashMap;
+import java.util.Map;
 
+public class LRUCache {
+	//DL Node
+	class DLinkNode {
+		int key;
+		int val;
+		DLinkNode next;
+		DLinkNode prev;
+	}
+
+	//implement methods
+	//1. addNode : add after head
+	public void addNode(DLinkNode node) {
+		node.prev = head;
+		node.next = head.next;
+		head.next.prev = node;
+		head.next = node;
+		
+	}
+	//2. removeNode
+	public void removeNode(DLinkNode node) {
+		DLinkNode prev = node.prev;
+		DLinkNode next = node.next;
+		
+		prev.next = next;
+		next.prev = prev;
+
+	}
+	//3. movetoHead
+	public void moveToHead(DLinkNode node) {
+		removeNode(node);
+		addNode(node);
+	}
+	//4.popTail
+	public DLinkNode popTail() {
+		DLinkNode node = tail.prev;
+		removeNode(node);
+        
+        return node;
+	}
+	
+	private int size;
+	private int capacity;
+	private Map<Integer,DLinkNode> cache;
+	private DLinkNode head, tail;
+	
+    public LRUCache(int capacity) {
+    	this.size = 0;
+    	this.capacity = capacity;
+    	cache = new HashMap<Integer, DLinkNode>();
+    	head = new DLinkNode();
+    	tail = new DLinkNode();
+    	
+    	head.next = tail;
+    	tail.prev = head;
+        
+    }
+    
+    public int get(int key) {
+        DLinkNode node = cache.get(key);
+        if(node == null) {
+        	return -1;
+        }
+        removeNode(node);
+        moveToHead(node);
+        return node.val;
+    }
+    
+    public void put(int key, int value) {
+    	//check if key present, if yes, update else follows
+    	//add to cache
+    	//move node to head
+    	//if size > capacity evict tail
+    	
+    	DLinkNode node = cache.get(key);
+    	
+    	if(node == null) {
+
+    		//new key
+    		node = new DLinkNode();
+    		node.key = key;
+    		node.val = value;
+    		
+    		//put key-value in hashmap
+    		cache.put(key, node);
+    		//add node
+    		addNode(node);
+    		size++;
+    		//check for size and popTail if needed
+    		if(size > capacity) {
+    			DLinkNode tail = popTail();
+    			cache.remove(tail.key);
+    			size--;
+    		}
+        } else {
+    		//update existing value
+    		cache.put(key,node);
+    		node.val = value;
+    		moveToHead(node);
+    	}        
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
