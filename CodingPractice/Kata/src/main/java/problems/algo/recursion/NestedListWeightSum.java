58762b2ffaa18cc5c3593fa4e8ba5f3ac1e37554
Anirban Ghosh <anirbanghosh@MacBook-Air.local>
2019-04-30 00:30:59 -0700

    add palindromic substrings

---
+++ CodingPractice/Kata/src/main/java/problems/algo/recursion/NestedListWeightSum.java
@@ -0,0 +1,14 @@
+package problems.algo.recursion;
+
+/*
+ * https://leetcode.com/problems/nested-list-weight-sum/
+ */
+public class NestedListWeightSum {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- CodingPractice/Kata/src/main/java/problems/algo/string/PalindromicSubstrings.java
+++ CodingPractice/Kata/src/main/java/problems/algo/string/PalindromicSubstrings.java
@@ -1,11 +1,17 @@
 package problems.algo.string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * https://leetcode.com/problems/palindromic-substrings/
  */
 public class PalindromicSubstrings {
 	
 	int count = 0;
+	List<String> strList = new ArrayList<String>();
+	
+	//Count of palindromic substrings
 	public int countSubstrings(String s) {
 
 		for (int i = 0; i < s.length(); i++) {
@@ -15,9 +21,19 @@ public class PalindromicSubstrings {
 
 		return count;
 	}
+	
+	public List<String> getAllSubstring(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			expand(s, i, i);
+			expand(s, i, i+1);
+		}
+		
+		return strList;
+	}
 
 	private void expand(String s, int l, int r) {
 		while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)){
+            strList.add(s.substring(l,r+1));
 			l--;
 			r++;
             count++;
@@ -27,6 +43,8 @@ public class PalindromicSubstrings {
 	public static void main(String[] args) {
 		PalindromicSubstrings p = new PalindromicSubstrings();
 		System.out.println(p.countSubstrings("aaabaaa"));
+		p.strList = new ArrayList<String>();
+		System.out.println(p.getAllSubstring("aaabaaa"));
 
 	}
 
